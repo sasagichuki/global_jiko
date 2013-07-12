451f9a6e9b43309b1bb3d20b3d91d82c8c19d9eb
sasagichuki@gmail.com <sasagichuki@gmail.com>
2013-07-12 14:47:18 +0300

    styled nav fake active - to do

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ $(".check").click(function() {
 //        alert($(this).val());
     }
 });
+
+// $(function () {
+//     setNavigation();
+// });
+
+// function setNavigation() {
+//     var path = window.location.pathname;
+//     path = path.replace(/\/$/, "");
+//     path = decodeURIComponent(path);
+
+//     $("nav a").each(function () {
+//         var href = $(this).attr('href');
+//         if (path.substring(0, href.length) === href) {
+//             $(this).closest('li').addClass('active');
+//         }
+//     });
+// }
